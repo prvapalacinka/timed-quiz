71628784a9fd91301353d0e799153309a45ab40a
Broly <vidovicp20@gmail.com>
2022-09-13 18:49:20 -0400

    JS Functions and vars

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # timed-quiz
+
+This project includes a timed quiz that shuffles through a variety of quiz questions upon the click of a start button that initiates a countdown timer. The timer decrements at a faster rate each time a question is answered wrong. 
+The score is displayed upon completion of the quiz.
+
--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 var generateBtn = document.querySelector("#start");
+var optionList = document.getElementById("optionList");
+var options = document.querySelectorAll(".option");
 var timeRemaining = 60;
+var question = document.getElementById("question");
+var optionOne = document.getElementById("option1");
+var optionTwo = document.getElementById("option2");
+var optionThree = document.getElementById("option3");
+var optionFour = document.getElementById("option4");
+
+/* counter variable */ 
+var counter = 1;
+
+/* Question arrays */ 
+var questionOne = [];
+questionOne.push("What is CSS used for?", 
+"To make Javascript look pretty.",
+"To design HTML elements on a webpage",
+"To hack into a database",
+"To save content on a webpage");
+
+var questionTwo = [];
+questionOne.push("What color is a tiger?", 
+"Green",
+"Orange and black",
+"Orange and blue",
+"Purple");
 
 function timerCountdown() {
     var newInterval = setInterval(function(){
@@ -9,5 +34,40 @@ function timerCountdown() {
   
 }
 
+function startQuiz() {
+    timerCountdown();
+    questionDisplay();
+  
+}
 
-generateBtn.addEventListener("click", timerCountdown);
+function questionDisplay() {
+    if (counter == 1) {
+    question.textContent = questionOne[0];
+    optionOne.textContent = questionOne[1];
+    optionTwo.textContent = questionOne[2];
+    optionThree.textContent = questionOne[3];
+    optionFour.textContent = questionOne[4];
+    }
+    else if (counter == 2) {
+        question.textContent = questionTwo[0];
+        optionOne.textContent = questionTwo[1];
+        optionTwo.textContent = questionTwo[2];
+        optionThree.textContent = questionTwo[3];
+        optionFour.textContent = questionTwo[4];
+    }
+}
+
+/* counter function */
+function counter() {
+    console.log("here");
+    counter+=1;
+    questionDisplay();
+}
+generateBtn.addEventListener("click", startQuiz);
+console.log(generateBtn);
+// options.addEventListener("click", counter)
+for(i=0; i<options.length; i++) {
+    console.log(options[i]);
+
+    options[i].addEventListener("click", counter)
+}
