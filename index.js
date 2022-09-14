1b6c1b6b2e33f558364b19779c9da5dd7c0e9c78
Broly <vidovicp20@gmail.com>
2022-09-14 12:48:25 -0400

    format and highscore button

--- index.js
+++ index.js
@@ -8,45 +8,45 @@ var optionTwo = document.getElementById("option2");
 var optionThree = document.getElementById("option3");
 var optionFour = document.getElementById("option4");
 
-/* counter variable */ 
+/* counter variable */
 var counter = 1;
 
-/* Question arrays */ 
+/* Question arrays */
 var questionOne = [];
-questionOne.push("What is CSS used for?", 
-"To make Javascript look pretty.",
-"To design HTML elements on a webpage",
-"To hack into a database",
-"To save content on a webpage");
+questionOne.push("What is CSS used for?",
+    "To make Javascript look pretty.",
+    "To design HTML elements on a webpage",
+    "To hack into a database",
+    "To save content on a webpage");
 
 var questionTwo = [];
-questionOne.push("What color is a tiger?", 
-"Green",
-"Orange and black",
-"Orange and blue",
-"Purple");
+questionOne.push("What color is a tiger?",
+    "Green",
+    "Orange and black",
+    "Orange and blue",
+    "Purple");
 
 function timerCountdown() {
-    var newInterval = setInterval(function(){
+    var newInterval = setInterval(function () {
         timeRemaining--;
-    document.getElementById("timeRemaining").textContent = timeRemaining;
+        document.getElementById("timeRemaining").textContent = timeRemaining;
     }, 1000)
-  
+
 }
 
 function startQuiz() {
     timerCountdown();
     questionDisplay();
-  
+
 }
 
 function questionDisplay() {
     if (counter == 1) {
-    question.textContent = questionOne[0];
-    optionOne.textContent = questionOne[1];
-    optionTwo.textContent = questionOne[2];
-    optionThree.textContent = questionOne[3];
-    optionFour.textContent = questionOne[4];
+        question.textContent = questionOne[0];
+        optionOne.textContent = questionOne[1];
+        optionTwo.textContent = questionOne[2];
+        optionThree.textContent = questionOne[3];
+        optionFour.textContent = questionOne[4];
     }
     else if (counter == 2) {
         question.textContent = questionTwo[0];
@@ -60,13 +60,13 @@ function questionDisplay() {
 /* counter function */
 function counter() {
     console.log("here");
-    counter+=1;
+    counter += 1;
     questionDisplay();
 }
 generateBtn.addEventListener("click", startQuiz);
 console.log(generateBtn);
 // options.addEventListener("click", counter)
-for(i=0; i<options.length; i++) {
+for (i = 0; i < options.length; i++) {
     console.log(options[i]);
 
     options[i].addEventListener("click", counter)
