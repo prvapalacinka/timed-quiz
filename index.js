ee79cf1e48363b499d83bb4f25e187c2ed5d7dd8
Broly <vidovicp20@gmail.com>
2022-10-05 12:34:37 -0400

    Score form div display

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ var score = 0;
 var correction = document.getElementById("correction");
 /* var for container on/off display */
 var containerDiv = document.getElementById("containerdiv");
-
+var formdiv = document.getElementById("form");
+formdiv.style.display = "none";
 /* counter variable */
 var counter = 0;
 var questions = [
@@ -41,52 +42,7 @@ var questions = [
         correctAnswer: "JS"
     },
 ]
-// /* Question arrays */
-// var questionOne = [];
-// questionOne.push("What is CSS used for?",
-//     "To make Javascript look pretty.",
-//     "To design HTML elements on a webpage",
-//     "To hack into a database",
-//     "To save content on a webpage");
 
-// var questionTwo = [];
-// questionTwo.push("What color is a tiger?",
-//     "Greenl alkdj k jk j",
-//     "Orange and blac adjflkjdfk",
-//     "Orange and blue kaldfajdlfj",
-//     "Purple jadkfjkadf");
-
-// var questionThree = [];
-// questionThree.push("What is HTML?",
-//     "The contents of a webpage",
-//     "A way to style the webpage",
-//     "How often a page receives info",
-//     "All of the above");
-
-// var questionFour = [];
-// questionFour.push("What is code?",
-//     "Something cool",
-//     "No idea",
-//     "Brother man",
-//     "The best thing ever");
-
-// var questionFive = [];
-// questionFive.push("What is banana?",
-//     "The best fruit",
-//     "A way of life",
-//     "Party time",
-//     "All of the above");
-
-
-// /* correct answers */
-// var answerKey = [];
-// answerKey.push("To design HTML elements on a webpage",
-//     "Greenl alkdj k jk j",
-//     "The contents of a webpage",
-//     "Something cool",
-//     "The best fruit");
-
-/* timer */
 function timerCountdown() {
     var newInterval = setInterval(function () {
         if (timeRemaining > 0) {
@@ -113,72 +69,8 @@ function questionDisplay() {
         element.textContent = questions[counter].answers[i];
         i++;
     })
-    // if (counter == 1) {
-    //     question.textContent = questions.question[0];
-    //     console.log(questions[1]);
-    // }
-    // else if (counter ==2) {
-    //     optionOne.textContent = questions[1];
-    // }
-    // else if (counter ==3) {
-    //     optionOne.textContent = questions[2];
-    // }
-    // else if (counter ==4) {
-    //     optionOne.textContent = questions[3];
-    // }
-    // else if (counter ==5) {
-    //     optionOne.textContent = questions[4];
-    // }
 }
 
-// function questionDisplay() {
-//     if (counter == 1) {
-//         question.textContent = questionOne[0];
-//         optionOne.textContent = questionOne[1];
-//         optionTwo.textContent = questionOne[2];
-//         optionThree.textContent = questionOne[3];
-//         optionFour.textContent = questionOne[4];
-//     }
-//     else if (counter == 2) {
-//         question.textContent = questionTwo[0];
-//         optionOne.textContent = questionTwo[1];
-//         optionTwo.textContent = questionTwo[2];
-//         optionThree.textContent = questionTwo[3];
-//         optionFour.textContent = questionTwo[4];
-//     }
-//     else if (counter == 3) {
-//         question.textContent = questionThree[0];
-//         optionOne.textContent = questionThree[1];
-//         optionTwo.textContent = questionThree[2];
-//         optionThree.textContent = questionThree[3];
-//         optionFour.textContent = questionThree[4];
-//     }
-//     else if (counter == 4) {
-//         question.textContent = questionFour[0];
-//         optionOne.textContent = questionFour[1];
-//         optionTwo.textContent = questionFour[2];
-//         optionThree.textContent = questionFour[3];
-//         optionFour.textContent = questionFour[4];
-//     }
-//     else if (counter == 5) {
-//         question.textContent = questionFive[0];
-//         optionOne.textContent = questionFive[1];
-//         optionTwo.textContent = questionFive[2];
-//         optionThree.textContent = questionFive[3];
-//         optionFour.textContent = questionFive[4];
-//     }
-//     else (containerDisplay);
-// }
-
-/* counter function */
-// function incrementCounter() {
-//     counter += 1;
-//     //console.log(options);
-//     var test = options[i];
-//     console.log(test);
-//     checkAnswer();
-//     questionDisplay();
-// }
 
 function checkAnswer(event) {
 let userAnswer = event.target.textContent
@@ -198,6 +90,7 @@ function containerDisplay() {
 
     document.getElementById("containerdiv").style.display = "none";
     document.getElementById("formdiv").style.display = "block";
+    formdiv.style.display = "block";
 
 
 }
